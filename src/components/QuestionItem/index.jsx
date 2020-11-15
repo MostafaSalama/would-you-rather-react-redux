e7d4b97725713f60bb68b45fe515e5db1b603313
Mostafa Salama <mostafatourad789@gmail.com>
2020-11-15 20:48:39 +0200

    feat: update the questionItem to include the user interface needed

--- src/components/QuestionItem/index.jsx
+++ src/components/QuestionItem/index.jsx
@@ -2,7 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './QuestionItem.module.css';
 function QuestionItem({ question }) {
-	return <div>{JSON.stringify(question)};</div>;
+	return (
+		<div className="flex">
+			<div className="w-300 2-300  question-container">
+				<div className="question-author-container">
+					<div className="question-image">
+						<img src={question.avatarURL} alt={question.author} />
+					</div>
+					<div className="question-author-title">
+						<p>{question.author}</p>
+					</div>
+				</div>
+				<div className="question">
+					<p className="title">Would You Rather</p>
+					<div>
+						<p>{question.optionOne.text}...</p>
+					</div>
+				</div>
+				<div className="btn-container">
+					<button className="btn btn-outline">View Question</button>
+				</div>
+			</div>
+		</div>
+	);
 }
 QuestionItem.propTypes = {
 	question: PropTypes.object,
